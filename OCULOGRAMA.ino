2c5f88d3e5d8626c14177c46a06b372469cf7d46
Bioelectronica Se escribe con B <35161121+BioelectronicaSeEscribeConB@users.noreply.github.com>
2018-08-14 00:18:23 -0500

    Add files via upload

---
+++ OCULOGRAMA.ino
@@ -0,0 +1,16 @@
+int lectura=A0;
+int lecturaOjo=0;
+
+void setup()
+{
+Serial.begin(9600);
+pinMode(lectura,INPUT);
+
+}
+
+void loop() 
+{
+ lecturaOjo=analogRead(lectura);
+ Serial.println(lecturaOjo);
+ delay(80);
+}
